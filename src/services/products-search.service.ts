9b8cd182cd7378084531fa6ae86199b3c063f598
jua15377 <jua15377@uvg.edu.gt>
2018-12-19 14:43:29 -0600

    fix: import name on product-search.service

--- src/services/products-search.service.ts
+++ src/services/products-search.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ResourceService, serializeCase ,serializeSnakeCase,QueryOptions } from './resource.service';
+import { ResourceService, serializeCase ,deserializeCase, QueryOptions } from './resource.service';
 import { HttpWrapper } from '../common/barrel';
 import { HttpResponse } from '@angular/common/http';
 import { urlConfig } from '../environments/config';
@@ -20,7 +20,7 @@ export class ProductSearchService extends ResourceService<ProductDto[]> {
     }
 
     getList(paramsObject: any = {}): Observable<ProductDto[]> {
-        const serializedSnakeCase =  serializeSnakeCase(paramsObject);
+        const serializedSnakeCase =  deserializeCase(paramsObject);
         const paramsQueryString = QueryOptions.toQueryString(serializedSnakeCase);
         const queryUrl = `${this.baseUrl}${paramsQueryString}`;
         return this.httpClient
